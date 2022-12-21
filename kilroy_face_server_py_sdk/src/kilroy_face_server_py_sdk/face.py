d44aa2691e41dd19662318ee8af99a7d29d0a284
Sebastian Pietras <01133337@pw.edu.pl>
2022-12-21 14:54:39 +0100

    Updated service code to match specification

--- kilroy_face_server_py_sdk/src/kilroy_face_server_py_sdk/face.py
+++ kilroy_face_server_py_sdk/src/kilroy_face_server_py_sdk/face.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
+from pathlib import Path
 from typing import Any, AsyncIterable, Dict, Generic, Optional, Tuple, TypeVar
 from uuid import UUID
 
@@ -10,22 +11,26 @@ StateType = TypeVar("StateType")
 
 
 class Face(Configurable[StateType], ABC, Generic[StateType]):
+    # noinspection PyMethodParameters
     @classproperty
     @abstractmethod
     def metadata(cls) -> Metadata:
         pass
 
+    # noinspection PyMethodParameters
     @classproperty
     @abstractmethod
     def post_schema(cls) -> JSONSchema:
         pass
 
     @abstractmethod
-    async def post(self, post: Dict[str, Any]) -> Tuple[UUID, Optional[str]]:
+    async def post(
+        self, content: Dict[str, Any]
+    ) -> Tuple[UUID, Optional[str]]:
         pass
 
     @abstractmethod
-    async def score(self, post_id: UUID) -> float:
+    async def score(self, id: UUID) -> float:
         pass
 
     @abstractmethod
@@ -36,3 +41,10 @@ class Face(Configurable[StateType], ABC, Generic[StateType]):
         after: Optional[datetime] = None,
     ) -> AsyncIterable[Tuple[UUID, Dict[str, Any], float]]:
         pass
+
+    async def reset_self(self) -> None:
+        await self.cleanup()
+        await self.init()
+
+    async def save_self(self, directory: Path) -> None:
+        await self.save(directory)
